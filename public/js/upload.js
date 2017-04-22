5ffa9bd7ccae67b4108d462de4a5658f4d863002
vincent10400094 <paintart.lin@gmail.com>
2017-04-23 01:21:36 +0800

    add back fileupload route

--- public/js/upload.js
+++ public/js/upload.js
@@ -9,65 +9,66 @@ var close = document.getElementById("close");
 // let widget = document.createElement('div');
 // widget.innerHTML = '<input type="file" name="pdf" id="fileElem1" class="form-control" onchange="upload(this.files)" style="display:none"><a href="#" id="fileSelect1" class="btn" style="width:80%;background-color:#ddd;text-transform:none">+</a>';
 
-function handleFiles1(files) {
+async function handleFiles1(files) {
 	console.log('1', files.length);
-    let file = files[0];
-    $('#fileSelect').hide();
+	let file = files[0];
+	$('#fileSelect').hide();
 	$('#title').text('Uploading ' + file.name);
-    $('#progressBar').show();
-    uploadFile(file);
-	$('#progressBar').hide();
-	$('#title').text('Succeed');
-	$('#close').show();
+	$('#progressBar').show();
+	uploadFile(file);
+	// $('#progressBar').hide();
+	// $('#title').text('Succeed');
+	// $('#close').show();
 }
 
 function handleFiles2(files) {
-  console.log('2', files.length);
-  fileSelect2.innerText = files[0].name;
+	console.log('2', files.length);
+	fileSelect2.innerText = files[0].name;
 }
 
 fileSelect1.addEventListener("click", (e) => {
-  console.log(e.target.id);
-  if (fileElem1 && e.target.id == 'fileSelect1') {
-    fileElem1.click();
-  }
-  e.preventDefault();
+	console.log(e.target.id);
+	if (fileElem1 && e.target.id == 'fileSelect1') {
+		fileElem1.click();
+	}
+	e.preventDefault();
 }, false);
 
 fileSelect2.addEventListener("click", (e) => {
-  console.log(e.target.id);
-  if (fileElem2 && e.target.id == 'fileSelect2') {
-    fileElem2.click();
-  }
-  e.preventDefault();
+	console.log(e.target.id);
+	if (fileElem2 && e.target.id == 'fileSelect2') {
+		fileElem2.click();
+	}
+	e.preventDefault();
 }, false);
 
 function uploadFile(file) {
-  var formData = new FormData();
-  formData.append('file', file);
-  var xhr = new XMLHttpRequest();
+	var formData = new FormData();
+	formData.append('file', file);
+	var xhr = new XMLHttpRequest();
 
-  // your url upload
-  xhr.open('post', '/uploadfile', true);
+	// your url upload
+	xhr.open('post', '/uploadfile', true);
 
-  xhr.upload.onprogress = function(e) {
-    let percentage;
-    if (e.lengthComputable) {
-      percentage = ((e.loaded / e.total) * 100).toString() + '%';
-      $('#percentage').css('width', percentage);
-      console.log(percentage);
-    }
-  };
+	xhr.upload.onprogress = function (e) {
 
-  xhr.onerror = function(e) {
-    console.log('Error');
-    console.log(e);
-  };
-  xhr.onload = function() {
-    console.log(this.statusText);
-  };
+		let percentage;
+		if (e.lengthComputable) {
+			percentage = ((e.loaded / e.total) * 100).toString() + '%';
+			$('#percentage').css('width', percentage);
+			console.log(percentage);
+		}
+	};
 
-  console.log(formData);
-  $('#percentage').css('width', '100%');
-  xhr.send(formData);
+	xhr.onerror = function (e) {
+		console.log('Error');
+		console.log(e);
+	};
+	xhr.onload = function () {
+		console.log(this.statusText);
+	};
+
+	console.log(formData);
+	xhr.send(formData);
+
 }
